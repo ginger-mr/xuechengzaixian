488dc618c2fcdb052fcb0f0eba95c2983300c853
ginger <1290089493@qq.com>
2020-09-21 17:24:21 +0800

    1、分页查询 CmsPage 集合下的数据。 2、根据站点 Id 、模板Id、页面别名查询页面信息。 3、接口基于 Http Get 请求，响应 Json 数据。
    根据需求定义接口。

---
+++ xcEduCode01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/request/QueryPageRequest.java
@@ -0,0 +1,18 @@
+package com.xuecheng.framework.domain.request;
+
+/**
+ * 封装查询条件
+ */
+public class QueryPageRequest {
+    //站点id
+    private String siteId;
+    //页面ID
+    private String pageId;
+    //页面名称
+    private String pageName;
+    //别名
+    private String pageAliase;
+    //模版id
+    private String templateId;
+    //...还可以添加查询条件
+}
