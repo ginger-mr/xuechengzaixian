8b2d32b11258cc093d0d9050b9f5cb11943fd7c4
ginger <1290089493@qq.com>
2020-09-22 17:48:31 +0800

    添加@GetMapping("/list")注解

--- xcEduCode01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/web/controller/CmsPageController.java
+++ xcEduCode01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/web/controller/CmsPageController.java
@@ -6,13 +6,17 @@ import com.xuecheng.framework.domain.request.QueryPageRequest;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
 public class CmsPageController implements CmsPageControllerApi {
+
+
     @Override
+    @GetMapping("/list")
     public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
         //暂时采用测试数据，测试接口是否可以正常运行。
         QueryResult<CmsPage> queryResult = new QueryResult<>();
